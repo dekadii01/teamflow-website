54fefe4ca12711d57e846bbd4d956f16b76a3303
adi pramana <kadekadi368@gmail.com>
2023-07-05 15:12:50 +0700

    update navbar fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,3 +40,21 @@ themeToggleBtn.addEventListener("click", function () {
 const htmlTahun = document.querySelector("#tahun");
 const tahunSekarang = new Date().getFullYear();
 htmlTahun.innerHTML = "©" + tahunSekarang;
+
+// Header on scroll
+
+window.onscroll = function () {
+  const header = document.querySelector("#header");
+  const fixNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
+
+  if (window.scrollY > fixNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("block");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.add("hidden");
+    toTop.classList.remove("block");
+  }
+};
